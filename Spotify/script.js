6be1358512ec68ce400bcc9fe55f5f840fca45de
devkrishnasharma <d22bca110563@cuchd.in>
2023-04-12 01:01:24 +0530

    update

--- Spotify/script.js
+++ Spotify/script.js
@@ -10,21 +10,24 @@ let songiteams = Array.from(document.getElementsByClassName('songIteam'));
 
 
 let songs = [ 
-   {songName: "Shape of You - Ed Shreen", filepath: "song/1.mp3", coverPath: "covers/1.jpg"}, 
-   {songName: "Suzume No Tojimari - Anime Movie", filepath: "song/1.mp3", coverPath: "covers/2.jpg"}, 
-   {songName: "Dandalions", filepath: "song/1.mp3", coverPath: "covers/3.jpg"}, 
-   {songName: "Middle of The Night", filepath: "song/1.mp3", coverPath: "covers/4.jpg"}, 
-   {songName: "Beggin", filepath: "song/1.mp3", coverPath: "covers/5.jpg"}, 
-   {songName: "Attention- Charlie Puth", filepath: "song/1.mp3", coverPath: "covers/6.jpg"}, 
-   {songName: "On MY Way- Alan Walker", filepath: "song/1.mp3", coverPath: "covers/7.jpg"}, 
-   {songName: "Blank Space- Tylor Swift", filepath: "song/1.mp3", coverPath: "covers/8.jpg"}, 
-   {songName: "Stereo Hearts- Glee Club", filepath: "song/1.mp3", coverPath: "covers/9.jpg"}, 
-   {songName: "Unholy", filepath: "song/1.mp3", coverPath: "covers/10.jpg"}, 
+   {songName: "Shape of You - Ed Shreen", filepath: "songs/1.mp3", coverPath: "covers/1.jpg"}, 
+   {songName: "Suzume No Tojimari - Anime Movie", filepath: "songs/1.mp3", coverPath: "covers/2.jpg"}, 
+   {songName: "Dandalions", filepath: "songs/1.mp3", coverPath: "covers/3.jpg"}, 
+   {songName: "Middle of The Night", filepath: "songs/1.mp3", coverPath: "covers/4.jpg"}, 
+   {songName: "Beggin", filepath: "songs/1.mp3", coverPath: "covers/5.jpg"}, 
+   {songName: "Attention- Charlie Puth", filepath: "songs/1.mp3", coverPath: "covers/6.jpg"}, 
+   {songName: "On MY Way- Alan Walker", filepath: "songs/1.mp3", coverPath: "covers/7.jpg"}, 
+   {songName: "Blank Space- Tylor Swift", filepath: "songs/1.mp3", coverPath: "covers/8.jpg"}, 
+   {songName: "Stereo Hearts- Glee Club", filepath: "songs/1.mp3", coverPath: "covers/9.jpg"}, 
+   {songName: "Unholy", filepath: "songs/1.mp3", coverPath: "covers/10.jpg"}, 
 
 ]
+
+console.log(songs);
 songiteams.forEach((element, i) => {
     console.log(element, i);
-    element.getElementsByTagName("img")[0].src = songs[i].filepath;
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
   });
 
 //audioElement.play()
@@ -54,4 +57,58 @@ audioElement.addEventListener('timeupdate', ()=>{
 
 myProgressBar.addEventListener('change', ()=>{
     audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
 })
