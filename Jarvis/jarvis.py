b6da99c9c40b9aa5850cd545ca2ed12987659607
devkrishnasharma <d22bca110563@cuchd.in>
2023-04-28 11:29:50 +0530

    update

--- Jarvis/jarvis.py
+++ Jarvis/jarvis.py
@@ -3,7 +3,7 @@ import datetime
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+engine.setProperty('voice', voices[0].id)
 
 
 
@@ -11,13 +11,14 @@ def speak(audio):
    engine.say(audio)
    engine.runAndWait()
 
-   def wishMe():
+def wishMe():
       hour = int(datetime.datetime.now().hour)
       if hour>=0 and hour<=12:
          speak("Good Morning!")
 
       elif hour>=12 and hour<=18:
          speak("Good Afternoon")
+
       else:
          speak("Good Evening")
 
