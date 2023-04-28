c6afcb8456040ded43f53967cf69b1e012aa207b
devkrishnasharma <d22bca110563@cuchd.in>
2023-04-28 11:15:31 +0530

    update

--- Jarvis/jarvis.py
+++ Jarvis/jarvis.py
@@ -1,11 +1,29 @@
 import pyttsx3
+import datetime
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-print(voices[1].id)
 engine.setProperty('voice', voices[1].id)
 
 
 
 def speak(audio):
-   pass
+   engine.say(audio)
+   engine.runAndWait()
+
+   def wishMe():
+      hour = int(datetime.datetime.now().hour)
+      if hour>=0 and hour<=12:
+         speak("Good Morning!")
+
+      elif hour>=12 and hour<=18:
+         speak("Good Afternoon")
+      else:
+         speak("Good Evening")
+
+      speak("I am Your Virtual Assistant. Please Tell Me How Can I Help You")     
+   
+
+
+if __name__ == "__main__":
+      wishMe()
