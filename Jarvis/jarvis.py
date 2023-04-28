6c3a83c2ad68cd9349cee88f25efba7a3401c0ba
devkrishnasharma <d22bca110563@cuchd.in>
2023-04-28 13:57:50 +0530

    update

--- Jarvis/jarvis.py
+++ Jarvis/jarvis.py
@@ -1,3 +1,4 @@
+from asyncore import read
 import pyttsx3
 import speech_recognition as sr
 import datetime
@@ -29,22 +30,20 @@ def wishMe():
 def takecommand():
 
    #It takes microphone input from the user  and resturn string output
-   r = sr.recognizers()
-   
+   r = sr.Recognizer()
    with sr.Microphone() as source:
         print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
-
    try:
-        print("Recognizing...")    
-        query = r.recognize_google(audio, language='en-in') #Using google for voice recognition.
-        print(f"User said: {query}\n")  #User query will be printed.
-
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+         
    except Exception as e:
-        # print(e)    
-        print("Say that again please...")   #Say that again will be printed in case of improper voice 
-        return "None" #None string will be returned
+      #print(e)
+        print("Say that again please...")
+        return "None" 
    return query
    
 
